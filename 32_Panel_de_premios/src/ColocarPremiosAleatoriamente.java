389c09dceb1343eb9766ff96430331fed11dc57c
manana <manana@A13E21>
2024-04-02 09:12:28 +0200

    ejercicio 1-04

---
+++ 32_Panel_de_premios/src/ColocarPremiosAleatoriamente.java
@@ -0,0 +1,33 @@
+
+public class ColocarPremiosAleatoriamente {
+
+	public static void main(String[] args) {
+		// Crear un panel de premios 10x10 que contine 5 premios
+		// en posiciones diferentes
+		
+		int [][]panelPremios= new int [10][10];
+		panelPremios=asignarPremio();
+		for (int i=0;i<panelPremios.length;i++) {
+			for(int j=0;j<panelPremios.length;j++) {
+				if (panelPremios[i][j]!=0) {
+					System.out.println("Hay premio en la posicion "+i+" "+j);
+				}
+			}
+		}
+	}
+	
+	static int [][] asignarPremio () {
+		int [][]panelPremios= new int [10][10];
+		int cont=0;
+		do {
+			int fila = (int)(Math.random ()*panelPremios.length);
+			int colum = (int)(Math.random ()*panelPremios.length);
+				if (panelPremios[fila][colum]!= 1) {
+					panelPremios[fila][colum]= 1;
+					cont++;
+				}
+		}while (cont<5);
+		return panelPremios;
+	
+	}
+}
