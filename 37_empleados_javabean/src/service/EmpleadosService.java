df85827c9164a166125a625c9799ab470dbbe43e
manana <manana@A13E21>
2024-04-04 13:16:01 +0200

    4-4

---
+++ 37_empleados_javabean/src/service/EmpleadosService.java
@@ -0,0 +1,52 @@
+package service;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import model.EmpleadosModel;
+
+public class EmpleadosService {
+	/*Realizar un programa para la gestión de empleados de una empresa. 
+	Un empleado se caracteriza por un nombre,un email, edad y tiene asociado un código de empleado.
+	al iniciar el programa, aparecerá el siguiente menú:
+	1.- Nuevo empleado
+	2.- Buscar empleado
+	3.- Eliminar empleado
+	4.- Mostrar datos de todos los empleados
+	5.- Salir
+	
+	1: Se pide el nómbre, email, edad y código del empleado. Si no existe empleado con ese código, se guarda, pero si el código ya existe, no se guarda y se muestra advertencia al usuario
+	2: Se pide el código del empleado y se muestran sus datos
+	3: Se pìde el código del empleado y se borra, indicando un mensaje que diga el nombre del empleado que se ha borrado
+	4.- Muestra datos de todos los empleados  */
+	
+	HashMap <Integer,EmpleadosModel> empleados = new HashMap <>();
+
+	
+	//*************************************************************************
+	public boolean agregarEmpleado (EmpleadosModel empleado) {
+		boolean existe=true;
+		if (!empleados.containsKey(empleado.getCodigo())) {
+			empleados.put(empleado.getCodigo(), empleado);
+			existe=false;
+		}
+		return existe;		
+	}
+	
+	//*************************************************************************
+	public EmpleadosModel bucarEmpleado (int codigo) {
+		return empleados.get(codigo); // devuelve nombre si existe, sino devuelve null
+	}
+	
+	//*************************************************************************
+	public EmpleadosModel eliminarEmpleado (int codigo) {
+		return empleados.remove(codigo);  //devuelve nombre si existe, sino devuelve null
+	}
+	
+	//*************************************************************************
+	public EmpleadosModel [] listadoEmpleados () {
+		Collection<EmpleadosModel> todos=empleados.values();
+		return todos.toArray(new EmpleadosModel[0]);	
+	}
+	
+}
