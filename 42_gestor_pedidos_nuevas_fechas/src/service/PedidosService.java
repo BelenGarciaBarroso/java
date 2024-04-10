56d28029eb7378612d8d0dd49abc8f1c2c5558f7
blngb <blngb@DESKTOP-PS8N365>
2024-04-10 16:33:07 +0200

    final

---
+++ 42_gestor_pedidos_nuevas_fechas/src/service/PedidosService.java
@@ -0,0 +1,67 @@
+package service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+
+import model.Pedido;
+
+public class PedidosService {
+	HashSet<Pedido> pedidos=new HashSet<>();
+	
+	public void nuevoPedido(Pedido pedido) {
+		pedidos.add(pedido);
+	}
+	
+	public Pedido pedidoMasReciente() {
+		Pedido pAux=null;
+		LocalDate fMax=LocalDate.of(0,1,1); //1/1/1970
+		for(Pedido p:pedidos) {
+			//si encontramos pedido con fecha más reciente que fMax
+			//actualizamos fMax y pAux
+			if(p.getFechaPedido().isAfter(fMax)) {
+				fMax=p.getFechaPedido();
+				pAux=p;
+			}
+		}
+		return pAux;
+	}
+	
+	public ArrayList<Pedido> pedidosEntreFechas(LocalDate f1, LocalDate f2) {
+		ArrayList<Pedido> aux=new ArrayList<Pedido>();
+		for(Pedido p:pedidos) {
+			//si fecha del pedido es posterior o igual a f1 y anterior o igual a f2, se incluye
+			if(p.getFechaPedido().compareTo(f1)>=0&&p.getFechaPedido().compareTo(f2)<=0) {
+				aux.add(p);
+			}
+		}
+		return aux;
+	}
+	
+	public Pedido pedidoCercano(LocalDate f1) {
+		Pedido aux=new Pedido();
+		long dias=0; 
+		long dias2=0;
+		for(Pedido p:pedidos) {
+			if (aux.getFechaPedido() == null) {
+				aux=p;
+				dias = Math.abs(ChronoUnit.DAYS.between(aux.getFechaPedido(), f1));
+				/*if (dias<0) {
+					dias=dias * -1;
+				}*/
+			}else {
+				dias2 = Math.abs(ChronoUnit.DAYS.between(p.getFechaPedido(), f1));
+				/*if (dias2<0) {
+					dias2=dias2 * -1;
+				}*/
+				if (dias2<dias) {
+					aux=p;
+					dias=dias2;
+				}
+			}
+		}
+		return aux;
+	}
+}
